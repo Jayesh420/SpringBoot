17363121fe0cccf9bcbf802828709eccf9e9e63a
jayesh tawade <tawade05@gmail.com>
2025-01-08 13:06:58 +0530

    project

---
+++ src/main/java/com/PrasadSir/AddDemo/Entity/Student.java
@@ -0,0 +1,45 @@
+package com.PrasadSir.AddDemo.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "StudentDemo")
+
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String schoolName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+
+}
---
+++ src/test/java/com/PrasadSir/AddDemo/AddDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PrasadSir.AddDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
